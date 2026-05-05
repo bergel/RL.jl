99f3edd198ab590220a835d7125ac8ce367fe6d5
Alexandre Bergel <alexandre.bergel@me.com>
2026-05-05 12:14:24 +0200

    Map and Car movement

--- src/RL.jl
+++ src/RL.jl
@@ -1,5 +1,60 @@
 module RL
 
-greet() = print("Hello World!")
+using StructEquality
+export State, display_map, new_car_pos
+export UP, DOWN, LEFT, RIGHT, ACTIONS, start_state
+
+ZOMBIE = "z"
+CAR = "c"
+ICE_CREAM = "i"
+EMPTY = "*"
+
+grid = [[ICE_CREAM, EMPTY],
+        [ZOMBIE, CAR]]
+
+@struct_hash_equal struct State
+    position::Tuple{Int, Int}
+    grid::Vector{Vector{String}}
+end
+
+function State(; position::Tuple{Int, Int}, grid::Vector{Vector{String}})
+    return State(position, grid)
+end
+
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
+ACTIONS = [UP, DOWN, LEFT, RIGHT]
+
+start_state = State((2, 2), grid)
+
+function new_car_pos(state::State, action)
+    row, col = state.position
+    if action == UP
+        return (max(row - 1, 1), col)
+    elseif action == DOWN
+        return (min(row + 1, length(state.grid)), col)
+    elseif action == LEFT
+        return (row, max(col - 1, 1))
+    elseif action == RIGHT
+        return (row, min(col + 1, length(state.grid[1])))
+    else
+        error("Invalid action")
+    end
+end
+
+function Base.show(io::IO, state::State)
+    println(io, "State(position=$(state.position), grid=[")
+    for row in state.grid
+        # println(join(row, ' '))
+        print(io, "    ")
+        print(io, row)
+        println(io, ",")
+    end
+    println(io, "])")
+end
+
 
 end # module RL
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,100 @@
-println("Testing...")
+using Test
+
+using RL
+
+ZOMBIE = "z"
+CAR = "c"
+ICE_CREAM = "i"
+EMPTY = "*"
+
+grid = [[ICE_CREAM, EMPTY],
+        [ZOMBIE, CAR]]
+
+grid2 = [[ICE_CREAM, ZOMBIE],
+        [ZOMBIE, CAR]]      
+
+@testset "State" begin
+    state = State((1, 2), grid)
+
+    @testset "Creation" begin
+        @test state.position == (1, 2)
+        @test state.grid == grid
+    end    
+
+    @testset "Comparison" begin
+        @testset "Equal" begin
+            state2 = State((1, 2), grid)
+            @test state == state2
+        end
+
+        @testset "Not Equal" begin
+            state3 = State((2, 1), grid)
+            @test state != state3
+        end
+
+        @testset "Not Equal" begin
+            state3 = State((1, 2), grid2)
+            @test state != state3
+        end        
+
+        @testset "Hash" begin
+            state2 = State((1, 2), grid)
+            @test hash(state) == hash(state2)
+
+            state3 = State((2, 1), grid)
+            @test hash(state) != hash(state3)
+        end
+
+        @testset "Hash" begin
+            state2 = State((1, 2), grid)
+            @test hash(state) == hash(state2)
+
+            state3 = State((2, 1), grid)
+            @test hash(state) != hash(state3)
+            
+            state4 = State((1, 2), grid2)
+            @test hash(state) != hash(state4)
+        end        
+    end
+
+    @testset "Display" begin
+        io = IOBuffer()
+        show(io, state)
+        output = String(take!(io))
+        @test output == """
+State(position=(1, 2), grid=[
+    ["i", "*"],
+    ["z", "c"],
+])
+"""
+    end 
+
+    @testset "new_car_pos" begin
+        state = State((2, 2), [[EMPTY, EMPTY, EMPTY, EMPTY], 
+                                [EMPTY, EMPTY, EMPTY, EMPTY], 
+                                [EMPTY, EMPTY, EMPTY, EMPTY], 
+                                [EMPTY, EMPTY, EMPTY, EMPTY]])
+
+        @testset "Expected Positions" begin
+            @test new_car_pos(state, UP) == (1, 2)
+            @test new_car_pos(state, DOWN) == (3, 2)
+            @test new_car_pos(state, LEFT) == (2, 1)
+            @test new_car_pos(state, RIGHT) == (2, 3)
+        end
+
+        @testset "Invalid Action" begin
+            @test_throws ErrorException new_car_pos(state, 99)
+        end
+
+        @testset "Boundary Conditions" begin
+            state = State((1, 1), grid)
+            @test new_car_pos(state, UP) == (1, 1)
+            @test new_car_pos(state, LEFT) == (1, 1)
+
+            state = State((2, 2), grid)
+            @test new_car_pos(state, DOWN) == (2, 2)
+            @test new_car_pos(state, RIGHT) == (2, 2)
+        end
+    end
+
+end
