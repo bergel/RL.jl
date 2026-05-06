2d46f638fc938391b0506b7f10e7c59654143128
Alexandre Bergel <alexandre.bergel@me.com>
2026-05-06 12:39:38 +0200

    Cleaned implementation and added the multi-armed bandit

--- src/RL.jl
+++ src/RL.jl
@@ -2,92 +2,20 @@ module RL
 
 using Random, StructEquality
 
-export State, new_car_pos, act
-export UP, DOWN, LEFT, RIGHT, ACTIONS, q, choose_action, train
+export CarState, new_car_pos, act
+export q, choose_action, train
 export RLContext
 
-ZOMBIE = "z"
-CAR = "c"
-ICE_CREAM = "i"
-EMPTY = "*"
-
-grid = [[ICE_CREAM, EMPTY],
-        [ZOMBIE, CAR]]
-
-@struct_hash_equal struct State
-    position::Tuple{Int, Int}
-    grid::Vector{Vector{String}}
-end
-
-function State(; position::Tuple{Int, Int}, grid::Vector{Vector{String}})
-    return State(position, grid)
-end
-
-UP = 0
-DOWN = 1
-LEFT = 2
-RIGHT = 3
-
-ACTIONS = [UP, DOWN, LEFT, RIGHT]
-
-function new_car_pos(state::State, action::Int)
-    row, col = state.position
-    if action == UP
-        return (max(row - 1, 1), col)
-    elseif action == DOWN
-        return (min(row + 1, length(state.grid)), col)
-    elseif action == LEFT
-        return (row, max(col - 1, 1))
-    elseif action == RIGHT
-        return (row, min(col + 1, length(state.grid[1])))
-    else
-        error("Invalid action $action. Valid actions are: $ACTIONS")
-    end
-end
-
-function Base.show(io::IO, state::State)
-    println(io, "State(position=$(state.position), grid=[")
-    for row in state.grid
-        print(io, "    ")
-        print(io, row)
-        println(io, ",")
-    end
-    println(io, "])")
-end
-
-function act(state::State, action::Int, io::IO=devnull)
-    reward = -1
-    is_done = false
-    new_grid = deepcopy(state.grid)
-    new_pos = new_car_pos(state, action)
-    old_pos = state.position
-    grid_item = state.grid[new_pos[1]][new_pos[2]]
-
-    if grid_item == ZOMBIE
-        println(io, "The car has been eaten by a zombie! Game over.")
-        is_done = true
-        reward = -100
-    elseif grid_item == ICE_CREAM
-        println(io, "The car has collected the ice cream! You win!")
-        is_done = true
-        reward = 1000
-    elseif grid_item == EMPTY
-        reward = -1  # Small negative reward for moving to an empty cell
-    elseif grid_item == CAR
-        reward = -1  # Small negative reward for moving to an empty cell
-    end
-    new_grid[new_pos[1]][new_pos[2]] = CAR  # Move the car to the new position
-    new_grid[old_pos[1]][old_pos[2]] = EMPTY  # Clear the old position
-
-    return State(position=new_pos, grid=new_grid), reward, is_done
-end
+abstract type AbstractState end
 
 struct RLContext
-    q_table::Dict{State, Vector{Float64}}
+    start_state::AbstractState
+    actions_count::Int
+
+    q_table::Dict{AbstractState, Vector{Float64}}
     alphas::Vector{Float64}
     gamma::Float64
     eps::Float64
-    start_state::State
 
     # How many episodes we are considering? More episodes means more exploration, and
     # therefore a more efficient model, but also more time to train.
@@ -100,7 +28,8 @@ struct RLContext
     seed::Int
 
     function RLContext(
-        start_state::State; 
+        start_state::AbstractState,
+        actions_count::Int; 
         min_alpha::Float64=0.02, 
         max_steps_per_episode::Int=20, 
         n_episodes::Int = 80, 
@@ -108,12 +37,16 @@ struct RLContext
         eps::Float64=0.1,
         seed::Int = 42
     )
+        q_table = Dict{AbstractState, Vector{Float64}}()
+        alphas = collect(range(1.0, stop=min_alpha, length=n_episodes))
         return new(
-            Dict{State, Vector{Float64}}(), 
-            collect(range(1.0, stop=min_alpha, length=n_episodes)), 
+            start_state,
+            actions_count,
+            q_table,
+            alphas, 
             gamma, 
             eps, 
-            start_state,
+            
             n_episodes,
             max_steps_per_episode,
             seed
@@ -121,9 +54,9 @@ struct RLContext
     end
 end
 
-function q(context::RLContext, state::State, action::Union{Int, Nothing}=nothing)
+function q(context::RLContext, state::AbstractState, action::Union{Int, Nothing}=nothing)
     if !(haskey(context.q_table, state))
-        context.q_table[state] = zeros(length(ACTIONS))
+        context.q_table[state] = zeros(context.actions_count)
     end
 
     if action === nothing
@@ -133,8 +66,8 @@ function q(context::RLContext, state::State, action::Union{Int, Nothing}=nothing
     end
 end
 
-function choose_action(context::RLContext, state::State)
-    rand() < context.eps && return rand(ACTIONS)
+function choose_action(context::RLContext, state::AbstractState)
+    rand() < context.eps && return rand(0:context.actions_count-1)
     return argmax(q(context, state)) - 1
 end
 
@@ -160,5 +93,99 @@ function train(context::RLContext, io::IO=stdout)
         println(io, "Episode $episode: Total Reward = $total_reward")
     end
 end
+
+
+################################################
+# CAR 
+ZOMBIE = "z"
+CAR = "c"
+ICE_CREAM = "i"
+EMPTY = "*"
+
+@struct_hash_equal struct CarState <: AbstractState
+    position::Tuple{Int, Int}
+    grid::Vector{Vector{String}}
+end
+
+function CarState(; position::Tuple{Int, Int}, grid::Vector{Vector{String}})
+    return CarState(position, grid)
+end
+
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
+ACTIONS = [UP, DOWN, LEFT, RIGHT]
+
+function new_car_pos(state::CarState, action::Int)
+    row, col = state.position
+    if action == UP
+        return (max(row - 1, 1), col)
+    elseif action == DOWN
+        return (min(row + 1, length(state.grid)), col)
+    elseif action == LEFT
+        return (row, max(col - 1, 1))
+    elseif action == RIGHT
+        return (row, min(col + 1, length(state.grid[1])))
+    else
+        error("Invalid action $action. Valid actions are: $ACTIONS")
+    end
+end
+
+function Base.show(io::IO, state::CarState)
+    println(io, "CarState(position=$(state.position), grid=[")
+    for row in state.grid
+        print(io, "    ")
+        print(io, row)
+        println(io, ",")
+    end
+    println(io, "])")
+end
+
+function act(state::CarState, action::Int, io::IO=devnull)
+    reward = -1
+    is_done = false
+    new_grid = deepcopy(state.grid)
+    new_pos = new_car_pos(state, action)
+    old_pos = state.position
+    grid_item = state.grid[new_pos[1]][new_pos[2]]
+
+    if grid_item == ZOMBIE
+        println(io, "The car has been eaten by a zombie! Game over.")
+        is_done = true
+        reward = -100
+    elseif grid_item == ICE_CREAM
+        println(io, "The car has collected the ice cream! You win!")
+        is_done = true
+        reward = 1000
+    elseif grid_item == EMPTY
+        reward = -1  # Small negative reward for moving to an empty cell
+    elseif grid_item == CAR
+        reward = -1  # Small negative reward for moving to an empty cell
+    end
+    new_grid[new_pos[1]][new_pos[2]] = CAR  # Move the car to the new position
+    new_grid[old_pos[1]][old_pos[2]] = EMPTY  # Clear the old position
+
+    return CarState(position=new_pos, grid=new_grid), reward, is_done
+end
+
+################################################
+# MULTI-ARMED BANDIT
+
+# In practice, We have only one state since the number of arms is fixed, but we can still
+# use the same structure for consistency with the rest of the code.
+struct MABState <: AbstractState 
+    arms_count::Int
+    MABState(arms_count::Int=3) = new(arms_count)
+end
+
+function act(state::MABState, action::Int, io::IO=devnull)
+    true_probs = [0.2, 0.5, 0.75]
+    reward = rand() < true_probs[action+1] ? 1 : 0
+    return state, reward, false
+end
+
+
 
 end # module RL
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,21 @@ CAR = "c"
 ICE_CREAM = "i"
 EMPTY = "*"
 
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
+ACTIONS = [UP, DOWN, LEFT, RIGHT]
+
 grid = [[ICE_CREAM, EMPTY],
         [ZOMBIE, CAR]]
 
 grid2 = [[ICE_CREAM, ZOMBIE],
         [ZOMBIE, CAR]]      
 
-@testset "State" begin
-    state = State((1, 2), grid)
+@testset "CarState" begin
+    state = CarState((1, 2), grid)
 
     @testset "Creation" begin
         @test state.position == (1, 2)
@@ -23,36 +30,36 @@ grid2 = [[ICE_CREAM, ZOMBIE],
 
     @testset "Comparison" begin
         @testset "Equal" begin
-            state2 = State((1, 2), grid)
+            state2 = CarState((1, 2), grid)
             @test state == state2
         end
 
         @testset "Not Equal" begin
-            state3 = State((2, 1), grid)
+            state3 = CarState((2, 1), grid)
             @test state != state3
         end
 
         @testset "Not Equal" begin
-            state3 = State((1, 2), grid2)
+            state3 = CarState((1, 2), grid2)
             @test state != state3
         end        
 
         @testset "Hash" begin
-            state2 = State((1, 2), grid)
+            state2 = CarState((1, 2), grid)
             @test hash(state) == hash(state2)
 
-            state3 = State((2, 1), grid)
+            state3 = CarState((2, 1), grid)
             @test hash(state) != hash(state3)
         end
 
         @testset "Hash" begin
-            state2 = State((1, 2), grid)
+            state2 = CarState((1, 2), grid)
             @test hash(state) == hash(state2)
 
-            state3 = State((2, 1), grid)
+            state3 = CarState((2, 1), grid)
             @test hash(state) != hash(state3)
             
-            state4 = State((1, 2), grid2)
+            state4 = CarState((1, 2), grid2)
             @test hash(state) != hash(state4)
         end        
     end
@@ -62,7 +69,7 @@ grid2 = [[ICE_CREAM, ZOMBIE],
         show(io, state)
         output = String(take!(io))
         @test output == """
-State(position=(1, 2), grid=[
+CarState(position=(1, 2), grid=[
     ["i", "*"],
     ["z", "c"],
 ])
@@ -70,7 +77,7 @@ State(position=(1, 2), grid=[
     end 
 
     @testset "new_car_pos" begin
-        state = State((2, 2), [[EMPTY, EMPTY, EMPTY, EMPTY], 
+        state = CarState((2, 2), [[EMPTY, EMPTY, EMPTY, EMPTY], 
                                 [EMPTY, EMPTY, EMPTY, EMPTY], 
                                 [EMPTY, EMPTY, EMPTY, EMPTY], 
                                 [EMPTY, EMPTY, EMPTY, EMPTY]])
@@ -87,11 +94,11 @@ State(position=(1, 2), grid=[
         end
 
         @testset "Boundary Conditions" begin
-            state = State((1, 1), grid)
+            state = CarState((1, 1), grid)
             @test new_car_pos(state, UP) == (1, 1)
             @test new_car_pos(state, LEFT) == (1, 1)
 
-            state = State((2, 2), grid)
+            state = CarState((2, 2), grid)
             @test new_car_pos(state, DOWN) == (2, 2)
             @test new_car_pos(state, RIGHT) == (2, 2)
         end
@@ -101,7 +108,7 @@ State(position=(1, 2), grid=[
         @testset "Move to ice cream cell" begin
             test_grid = [[EMPTY, ICE_CREAM],
                          [ZOMBIE, CAR]]
-            state = State((2, 2), test_grid)
+            state = CarState((2, 2), test_grid)
             result, _, _ = act(state, UP)
 
             @test result.position == (1, 2)
@@ -112,7 +119,7 @@ State(position=(1, 2), grid=[
         @testset "Move to zombie cell" begin
             test_grid = [[EMPTY, ICE_CREAM],
                          [ZOMBIE, CAR]]
-            state = State((2, 2), test_grid)
+            state = CarState((2, 2), test_grid)
 
             result, _, _ = act(state, LEFT)
             @test result.position == (2, 1)
@@ -123,7 +130,7 @@ State(position=(1, 2), grid=[
         @testset "Move to ice cream cell" begin
             test_grid = [[CAR, ICE_CREAM],
                          [EMPTY, EMPTY]]
-            state = State((1, 1), test_grid)
+            state = CarState((1, 1), test_grid)
 
             result, _, _ = act(state, RIGHT)
             @test result.position == (1, 2)
@@ -135,12 +142,12 @@ State(position=(1, 2), grid=[
 end
 
 @testset "q" begin
-    state = State((1, 1), [[EMPTY, ICE_CREAM], [ZOMBIE, CAR]])
-    context = RLContext(state)
+    state = CarState((1, 1), [[EMPTY, ICE_CREAM], [ZOMBIE, CAR]])
+    context = RLContext(state, 4)
     @testset "Initialization" begin
         q_values = RL.q(context, state)
-        @test length(q_values) == length(RL.ACTIONS)
-        @test q_values == zeros(length(RL.ACTIONS))
+        @test length(q_values) == 4
+        @test q_values == zeros(4)
     end
 
     @testset "Action lookup" begin
@@ -152,24 +159,24 @@ end
 end
 
 @testset "choose_action" begin
-    state = State((1, 1), [[EMPTY, ICE_CREAM], [ZOMBIE, CAR]])
+    state = CarState((1, 1), [[EMPTY, ICE_CREAM], [ZOMBIE, CAR]])
 
     @testset "Epsilon-greedy" begin
         # Set eps to 1 to always choose a random action
-        context = RLContext(state; eps=1.0)
+        context = RLContext(state, 4; eps=1.0)
         random_actions = [choose_action(context, state) for _ in 1:100]
-        @test all(action -> action in RL.ACTIONS, random_actions)
+        @test all(action -> action in 0:3, random_actions)
 
-        context = RLContext(state; eps=0.0)
+        context = RLContext(state, 4; eps=0.0)
         @test choose_action(context, state) == 0
     end
 end
 
 @testset "train" begin
-    start_state = State((2, 2), [[EMPTY, ICE_CREAM], [ZOMBIE, CAR]])
-    context = RLContext(start_state)
+    start_state = CarState((2, 2), [[EMPTY, ICE_CREAM], [ZOMBIE, CAR]])
+    context = RLContext(start_state, 4)
     train(context, devnull)
 
-    @test q(context, start_state) != zeros(length(RL.ACTIONS))
+    @test q(context, start_state) != zeros(4)
     @test argmax(q(context, start_state)) == 1
 end
