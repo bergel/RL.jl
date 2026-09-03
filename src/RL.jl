06b1f979567b8940ad7e999fe4eae0434b0892ee
Alexandre Bergel <alexandre.bergel@me.com>
2026-09-03 07:59:09 +0200

    Added more examples

--- src/RL.jl
+++ src/RL.jl
@@ -4,7 +4,7 @@ using Random, StructEquality
 
 export CarState, new_car_pos, act
 export q, choose_action, train
-export RLContext
+export RLContext, NGGState
 
 abstract type AbstractState end
 
@@ -13,8 +13,19 @@ struct RLContext
     actions_count::Int
 
     q_table::Dict{AbstractState, Vector{Float64}}
+
+    # Learning rate for Q-learning. Higher alpha means that the agent learns more from new
+    # experiences, while lower alpha means that it relies more on past knowledge. We can
+    # use a schedule of alphas that decrease over time to allow for more exploration in
+    # the beginning and more exploitation later on.
     alphas::Vector{Float64}
+
+    # Discount factor for future rewards. Higher gamma means that the agent values future
+    # rewards more, while lower gamma means that it values immediate rewards more.
     gamma::Float64
+
+    # Epsilon for epsilon-greedy action selection. Higher epsilon means more exploration,
+    # but also more randomness in the actions taken by the agent.
     eps::Float64
 
     # How many episodes we are considering? More episodes means more exploration, and
@@ -62,13 +73,13 @@ function q(context::RLContext, state::AbstractState, action::Union{Int, Nothing}
     if action === nothing
         return context.q_table[state]
     else
-        return context.q_table[state][action + 1]
+        return context.q_table[state][action]
     end
 end
 
 function choose_action(context::RLContext, state::AbstractState)
-    rand() < context.eps && return rand(0:context.actions_count-1)
-    return argmax(q(context, state)) - 1
+    rand() < context.eps && return rand(1:context.actions_count)
+    return argmax(q(context, state))
 end
 
 function train(context::RLContext, io::IO=stdout)
@@ -83,7 +94,7 @@ function train(context::RLContext, io::IO=stdout)
             action = choose_action(context, state)
             new_state, reward, is_done = act(state, action, io)
 
-            q(context, state)[action + 1] += alpha * (reward + context.gamma * maximum(q(context, new_state)) - q(context, state, action))
+            q(context, state)[action] += alpha * (reward + context.gamma * maximum(q(context, new_state)) - q(context, state, action))
 
             state = new_state
             total_reward += reward
@@ -111,10 +122,10 @@ function CarState(; position::Tuple{Int, Int}, grid::Vector{Vector{String}})
     return CarState(position, grid)
 end
 
-UP = 0
-DOWN = 1
-LEFT = 2
-RIGHT = 3
+UP = 1
+DOWN = 2
+LEFT = 3
+RIGHT = 4
 
 ACTIONS = [UP, DOWN, LEFT, RIGHT]
 
@@ -181,11 +192,71 @@ struct MABState <: AbstractState
 end
 
 function act(state::MABState, action::Int, io::IO=devnull)
+    @assert action >= 1 && action <= state.arms_count "Action must be between 1 and $(state.arms_count)"
     true_probs = [0.2, 0.5, 0.75]
-    reward = rand() < true_probs[action+1] ? 1 : 0
+    reward = rand() < true_probs[action] ? 1 : 0
     return state, reward, false
 end
 
+################################################
+# Number Guessing Game
+# context = RLContext(NGGState(1), 100; max_steps_per_episode = 1000, n_episodes=10000, eps=0.1)
+# train(context)
+# argmax(q(context, NGGState(1)))  # Should show higher values for actions 6 and 7
 
+@struct_hash_equal struct NGGState <: AbstractState
+    current_guess::Int
+end 
+
+function act(state::NGGState, action::Int, io::IO=devnull)
+    target = 7
+    #is_done = action == target
+    is_done = false  # We want the agent to keep learning even after finding the correct answer
+
+    reward = action == target ? 10 : -1
+    # reward = state.current_guess == target ? 10 : -1
+
+    # new guess is the action taken by the agent
+    return NGGState(action), reward, is_done
+end
+
+################################################
+# TIC TAC TOE
+
+include("tictactoe.jl")
+
+
+################################################
+# Dynamic Pricing for a Small Hotel
+
+# @struct_hash_equal struct DPSHState <: AbstractState
+#     day_of_week::Int,
+#     rooms_left::Int,
+#     season::String,
+#     competitor_price_bucket::String
+# end 
+
+# function act(state::DPSHState, action::Int, io::IO=devnull)
+#     room_price[80, 100, 120, 140, 160][action]  # Map action to a price
+#     # Simulate demand based on the state and the price
+#     demand = simulate_demand(state, room_price)
+#     # revenue = min(demand, state.rooms_left) * room_price
+#     reward = rooms_sold * room_price
+
+#     booking_probability =
+#         sigmoid(
+#             3
+#             - 0.02 * price
+#             + 0.5 * weekend
+#             + 0.8 * summer
+#         )
+
+
+#     new_rooms_left = max(state.rooms_left - demand, 0)
+
+#     new_state = DPSHState(state.day_of_week, new_rooms_left, state.season, state.competitor_price_bucket)
+
+#     return new_state, reward, new_rooms_left == 0
+# end
 
 end # module RL
